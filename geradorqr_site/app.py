4e9a3d0be7a0aa95cc231190795f7c1974dee28f
João Vitor Mesquita de Lima <99664194+joaomesquitalima@users.noreply.github.com>
2024-01-22 19:38:30 -0400

    Update app.py

--- geradorqr_site/app.py
+++ geradorqr_site/app.py
@@ -14,7 +14,7 @@ def processar_formulario():
     # Obtém o texto do formulário enviado pelo usuário
     texto_do_usuario = request.form['texto_usuario']
     nome_arquivo = request.form['arquivo']
-    cor = request.form['cor_escolhida']
+    
     
     qr = qrcode.QRCode(
         version=1,
@@ -22,28 +22,21 @@ def processar_formulario():
         box_size=10,
         border=4,
     )
-    
-
     # Adiciona os dados ao QRCode
     qr.add_data(texto_do_usuario)
     qr.make(fit=True)
 
     # Cria uma imagem QRCode
-    img_qrcode = qr.make_image(fill_color=f"{cor}", back_color="white")
+    img_qrcode = qr.make_image(fill_color=f"#000000", back_color="#ffffff")
     img_qrcode = img_qrcode.convert('RGB')
     img_qrcode = np.array(img_qrcode)
+    img_qrcode = cv.cvtColor(img_qrcode,cv.COLOR_RGB2BGR)
     
     retval, buffer = cv.imencode('.png', img_qrcode)
-    buffer64 = base64.b64encode(buffer)
-    
-    
+    buffer64 = base64.b64encode(buffer)  
     string64 = buffer64.decode('utf-8')
-    
     
     return render_template('imagem.html',imagem_base64=string64,nome_arq=nome_arquivo)
 
-    
-
-
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
